545cc9db0c97f10983355bd37700116e256339f9
Mitchell <17392435+MitchellBerry@users.noreply.github.com>
2020-02-11 12:10:49 +0800

    updated write to file example

--- src/response.rs
+++ src/response.rs
@@ -17,15 +17,19 @@ impl Response {
   /// Writes the image response to a file at the specified relative or absolute path
   /// 
   ///  # Example
-  /// ```
-  /// let client = Client::new();
-  /// let api = Image::new("https://ids.lib.harvard.edu/ids/iiif/");
-  /// api.identifier = "25286607".into();
-  /// api.request(&client)
-  ///     .await
-  ///     .write_to_file("foo.jpg")
-  ///     .await
-  ///     .expect("Writing file to disk");
+  /// ```rust,no_run
+  /// #[tokio::main]
+  /// async fn main() {
+  ///   use iiif::*;
+  ///   let client = Client::new();
+  ///   let mut api = Image::new("https://ids.lib.harvard.edu/ids/iiif/");
+  ///   api.identifier = "25286607".into();
+  ///   api.request(&client)
+  ///       .await
+  ///       .write_to_file("foo.jpg")
+  ///       .await
+  ///       .expect("Writing file to disk");
+  /// }
   /// ```
   pub async fn write_to_file(self, path: &str) -> std::io::Result<()>{
     let mut file = File::create(path).unwrap();
