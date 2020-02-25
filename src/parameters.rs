6e8f74053aa59e35587d298130c899c1224916ba
Mitchell <17392435+MitchellBerry@users.noreply.github.com>
2020-02-25 18:23:41 +0800

    moved tests

--- src/parameters.rs
+++ src/parameters.rs
@@ -76,6 +76,9 @@ pub enum Rotation {
 }
 
 /// The quality parameter determines whether the image is delivered in color, grayscale or black and white.
+
+
+
 /// 
 /// The default is Quality::ServerDefault
 /// 
@@ -202,4 +205,29 @@ impl Default for Quality {
 
 impl Default for Format {
   fn default() -> Self { Format::Jpg }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  #[test]
+  fn absolute_to_string() {
+    let abs = Absolute {
+      x: 1,
+      y: 2,
+      w: 3,
+      h: 4
+    };
+    assert_eq!("1,2,3,4", abs.to_string());
+  }
+  #[test]
+  fn percentage_to_string() {
+    let pct = Percentage {
+      x: 1.2345, 
+      y: 2f32, 
+      w: 3.03, 
+      h: 4.0
+    };
+    assert_eq!("pct:1.234,2,3.03,4", pct.to_string());
+  }
 }
