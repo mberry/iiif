0c444c0458608abdd404a1e99920a45047d5265d
Mitchell <17392435+MitchellBerry@users.noreply.github.com>
2020-02-25 18:22:53 +0800

    move tests / rename modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,15 @@
 mod url;
 mod utils;
 mod api;
-mod request;
-mod response;
+mod requests;
+mod responses;
 mod parameters;
 
 use utils::*;
 use bytes::Bytes;
 pub use api::*;
-pub use request::*;
-pub use response::*;
+pub use requests::*;
+pub use responses::*;
 pub use parameters::*;
 pub use reqwest::{Client, Url};
 
-
-#[cfg(test)]
-mod tests {
-  use super::*;
-  #[test]
-  fn absolute_to_string() {
-    let abs = Absolute {x: 1,y: 2,w: 3,h: 4};
-    assert_eq!("1,2,3,4", abs.to_string());
-  }
-  #[test]
-  fn percentage_to_string() {
-    let pct = Percentage {x: 1.2345,y: 2f32,w: 3.03,h: 4.0};
-    assert_eq!("pct:1.234,2,3.03,4", pct.to_string());
-  }
-}
