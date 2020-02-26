1ce2d53ce566d2c17f99d9ab26447a659662b399
Mitchell <17392435+MitchellBerry@users.noreply.github.com>
2020-02-26 10:04:53 +0800

    updated example

--- examples/get_info.rs
+++ examples/get_info.rs
@@ -5,7 +5,8 @@ async fn main() {
   let mut api = Image::new("https://ids.lib.harvard.edu/ids/iiif");
   api.identifier("25286607");
   let info = api.fetch_info()
-                .await;
+                .await
+                .unwrap();
   dbg!(info);
 }
 
