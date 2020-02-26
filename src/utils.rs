486b9dc1a0915d6d86168fc35fc3233b38675cf5
Mitchell <17392435+MitchellBerry@users.noreply.github.com>
2020-02-26 10:00:41 +0800

    module comments

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,4 @@
+//! Helper functions
 
 // Limit float resolution down to 3 decimal places
 pub(crate) fn format_floats<T: std::fmt::Display>(floats: Vec<T>) -> Vec<String> {
@@ -6,6 +7,7 @@ pub(crate) fn format_floats<T: std::fmt::Display>(floats: Vec<T>) -> Vec<String>
         .collect()
 }
 
+// Convert coords to iiif parameter string
 pub(crate) fn join_coords<T: ToString>(x: T, y: T, w: T, h: T) -> String {
   [x, y, w, h].iter()
               .map(|s| s.to_string())
